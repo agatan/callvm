82d3a3730c60eee72138059382f2448168b31f64
agatan <agatan039@gmail.com>
2015-06-29 18:38:35 +0900

    fix double parser

--- src/callvm/parser.cpp
+++ src/callvm/parser.cpp
@@ -31,9 +31,10 @@ class callvm_grammar
    public:
     callvm_grammar() : callvm_grammar::base_type(expression) {
         int_expr = qi::int_[_val = phx::construct<ast::int_expr>(_1)];
-        double_expr = qi::double_[_val = phx::construct<ast::double_expr>(_1)];
-        primary %= (double_expr - qi::int_) | int_expr |
-                   ('(' >> expression >> ')')[_val = _1];
+        double_expr = qi::real_parser<double, qi::strict_real_policies<double>>()
+            [_val = phx::construct<ast::double_expr>(_1)];
+        primary %=
+            double_expr | int_expr | ('(' >> expression >> ')')[_val = _1];
         mul_expr =
             primary[_val = _1] >>
             *(('*' >>
